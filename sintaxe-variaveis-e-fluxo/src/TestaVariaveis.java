3d8fee9c270c4f842ca8fe6dac97357e1687c4d8
LeonardoCamboim <almleon@gmail.com>
2021-06-05 12:40:52 -0300

    Push inicial

---
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,16 @@
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		System.out.println("Novo teste");
+		
+		int idade;
+		idade = 30;
+		System.out.println(idade);
+		
+		idade = 30 + 10;
+		
+		System.out.println("Sua idade é: " + idade);
+	}
+	
+}
---
+++ sintaxe-variaveis-e-fluxo/src/TestePontoFlutuante.java
@@ -0,0 +1,10 @@
+
+public class TestePontoFlutuante {
+	
+	public static void main(String[] args) {
+		
+		double salario = 1250.6;
+		System.out.println("O salario é: " + salario);
+		
+	}
+}
