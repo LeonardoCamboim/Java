509e3dc050bc08445f1d6a189eae8f76ddf54c37
LeonardoCamboim <almleon@gmail.com>
2022-08-30 14:57:27 -0300

    Atividade java TED

---
+++ Faculdade/src/Atividades/AtributoSenha.java
@@ -0,0 +1,15 @@
+package Atividades;
+
+public class AtributoSenha {
+
+	private int senha;
+
+	public int getSenha() {
+		return senha;
+	}
+
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+	
+}
