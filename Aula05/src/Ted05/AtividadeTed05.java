9b8476b29e67556a9246e44c11bcfcbcb87bcff2
almle <almle@DESKTOP-081M0JS>
2022-09-11 18:33:45 -0300

    Atividade Ted 05

---
+++ Aula05/src/Ted05/AtividadeTed05.java
@@ -0,0 +1,22 @@
+package Ted05;
+
+public class AtividadeTed05 {
+
+	public void questaoUm() {
+		
+		int i;
+		i = 1;
+		
+		while (i <= 20) {
+			
+			if (i % 5 != 0) {
+				System.out.print(i);
+				System.out.print('\t');
+			} else {
+				System.out.println(i);
+			}
+			
+			i++;
+		}
+	}
+}
