e48316736928ddcc94d3db7eca6b802f86bc2b5d
Leonardo <almleon@gmail.com>
2022-10-24 17:53:23 -0300

    Alterações

--- ProjetoIntegrador/src/PetDream/Veterinario.java
+++ ProjetoIntegrador/src/PetDream/Veterinario.java
@@ -5,11 +5,23 @@ public class Veterinario extends Pessoa {
 	private int matricula;
 	private Double salario;
 
-	public Veterinario(String nome, String cpf, String endereço, String cidade, String estado, String telefoneFixo, String celular, int matricula, Double salario) {
+		public Veterinario(String nome, String cpf, String endereço, String cidade, String estado, String telefoneFixo, String celular, int matricula, Double salario) {
 		// TODO Auto-generated constructor stub
 		super(nome, cpf, endereço, cidade, estado, telefoneFixo, celular);
 		this.matricula = matricula;
 		this.salario = salario;
+	}
+		
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+
+	public int getMatricula() {
+		return matricula;
 	}
 
 }
